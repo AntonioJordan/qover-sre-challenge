aed38053dc7e19d148b71b61d87c13eeb0b528d4
Toni <tonijordanrodriguez6@gmail.com>
2026-02-02 01:23:20 +0100

    feat(secrets): validate MongoDB URI injection and authentication via Kubernetes Secret

--- infra/secret.tf
+++ infra/secret.tf
@@ -5,8 +5,9 @@ resource "kubernetes_secret_v1" "mongo_auth" {
   }
 
   data = {
-    MONGO_INITDB_ROOT_USERNAME = base64encode(var.mongo_user)
-    MONGO_INITDB_ROOT_PASSWORD = base64encode(var.mongo_password)
+    MONGO_INITDB_ROOT_USERNAME = var.mongo_user
+    MONGO_INITDB_ROOT_PASSWORD = var.mongo_password
+    MONGO_URI = "mongodb://${var.mongo_user}:${var.mongo_password}@mongo-0.mongo-headless.qover.svc.cluster.local:27017/admin"
   }
 
   type = "Opaque"
